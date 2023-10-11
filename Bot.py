fa3625b912d27bf6bfc63c3d67ea8e38111e6587
Project INTA <dimonkutsy@gmail.com>
2023-10-11 14:41:37 +0300

    main

--- Bot.py
+++ Bot.py
@@ -9,9 +9,9 @@ def start_message(message):
   bot.send_message(message.chat.id,"Я бот для графиков, пока ничаго не умею.")
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, """\
+    bot.send_message(message, """\
 Я пока ничаго не умею))\
 """)
 
@@ -19,7 +19,7 @@ def send_welcome(message):
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, 'Отстань')
+    bot.send_message(message, 'Отстань')
 
 
 bot.infinity_polling()
