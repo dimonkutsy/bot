022710d65a83ca2571414bb4935f7c85e0251bc7
dimonkutsy <dimonkutsy@gmail.com>
2023-11-29 10:50:02 +0300

    auth-update

--- Bot.py
+++ Bot.py
@@ -1,66 +1,114 @@
 import telebot
-from telebot import types
 import sqlite3
+import re
 
 bot = telebot.TeleBot('6453843079:AAFHVRXmYAKhssF8INHw6h4WfQ1eZv9pu_I')
-name = None
-
 
 @bot.message_handler(commands=['start'])
-def start(message):  
+def start(message):
     conn = sqlite3.connect('bd.sql')
     cur = conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50), pass varchar(50))')
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(50), surname VARCHAR(50), pass VARCHAR(50), version INTEGER DEFAULT 0)')
     conn.commit()
     cur.close()
     conn.close()
-    bot.send_message(message.chat.id, 'Введите ваше имя')
+    bot.send_message(message.chat.id, 'Введи своё имя 👾')
     bot.register_next_step_handler(message, user_name)
+
 def user_name(message):
-    global name
     name = message.text.strip()
-    bot.send_message(message.chat.id, 'Введите пароль')
-    bot.register_next_step_handler(message, user_pass)   
-def user_pass(message):
-    password = message.text.strip()
+
+    if not validate_russian(name):
+        bot.reply_to(message, 'Имя может содержать только русские буквы. Попробуй еще раз.')
+        bot.register_next_step_handler(message, user_name)
+        return
+
+    bot.send_message(message.chat.id, 'А теперь фамилию 👾')
+    bot.register_next_step_handler(message, user_surname, name)
+
+def user_surname(message, name):
+    surname = message.text.strip()
+
+    if not validate_russian(surname):
+        bot.reply_to(message, 'Фамилия может содержать только русские буквы. Попробуй еще раз.')
+        bot.register_next_step_handler(message, user_surname, name)
+        return
+
+    user_exists = check_user_exists(name, surname)
+
+    if user_exists:
+        bot.send_message(message.chat.id, 'Пользователь с такими данными уже зарегистрирован. Попробуй еще раз.')
+        bot.register_next_step_handler(message, user_name)
+    else:
+        save_user_data(name, surname)
+        bot.send_message(message.chat.id, 'Регистрация успешна!')
+        # В этом месте данные пользователя успешно сохранены в базе данных
+
+def validate_russian(text):
+    return bool(re.match(r'^[а-яА-ЯёЁ\s]+$', text))
+
+def check_user_exists(name, surname):
     conn = sqlite3.connect('bd.sql')
     cur = conn.cursor()
-    cur.execute("INSERT INTO users (name, pass) VALUES ('%s', '%s')" % (name, password))
+    cur.execute("SELECT * FROM users WHERE name = ? AND surname = ?", (name, surname))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    return user is not None
+
+def save_user_data(name, surname):
+    conn = sqlite3.connect('bd.sql')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users (name, surname) VALUES (?, ?)", (name, surname))
     conn.commit()
     cur.close()
     conn.close()
 
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Список пользователей', callback_data='users'))
-    bot.send_message(message.chat.id, 'Пользователь зарегистрирован!', reply_markup=markup)
-    
-@bot.message_handler(content_types=['photo'])
-def get_photo(message):
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton('Покажи график!', url='https://music.yandex.ru/users/cutsiy.d/playlists/3')
-    markup.row(btn1)
-    btn2 = types.InlineKeyboardButton('Изменить!', callback_data='edit')
-    btn3 = types.InlineKeyboardButton('Удалить!', callback_data='delete')
-    markup.row(btn2, btn3)
-    bot.reply_to(message, 'Бот график работ!', reply_markup=markup)
+@bot.message_handler(commands=['auth'])
+def authenticate(message):
+    bot.send_message(message.chat.id, 'Введите имя пользователя 👤')
+    bot.register_next_step_handler(message, check_username)
 
+def check_username(message):
+    username = message.text.strip()
 
-@bot.callback_query_handler(func=lambda callback: True)
-def callback(callback):
+    if username == 'admin':
+        bot.send_message(message.chat.id, 'Введите пароль 🔐')
+        bot.register_next_step_handler(message, check_password)
+    else:
+        bot.send_message(message.chat.id, 'Неверное имя пользователя. Попробуйте еще раз.')
+        bot.register_next_step_handler(message, check_username)
+
+def check_password(message):
+    password = message.text.strip()
+
+    if password == 'admin':
+        markup = telebot.types.ReplyKeyboardMarkup()
+        markup.add(telebot.types.KeyboardButton('Список пользователей 📋'))
+        bot.send_message(message.chat.id, 'Вы успешно авторизованы! Выберите действие:', reply_markup=markup)
+        bot.register_next_step_handler(message, handle_admin_action)
+    else:
+        bot.send_message(message.chat.id, 'Неверный пароль. Попробуйте еще раз.')
+        bot.register_next_step_handler(message, check_password)
+
+def handle_admin_action(message):
+    if message.text == 'Список пользователей 📋':
+        users = get_user_list()
+        if not users:
+            bot.send_message(message.chat.id, 'На данный момент нет зарегистрированных пользователей.')
+        else:
+            user_list = '\n'.join(users)
+            bot.send_message(message.chat.id, f'Список зарегистрированных пользователей:\n{user_list}')
+    else:
+        bot.send_message(message.chat.id, 'Неверное действие. Выберите корректную опцию.')
+
+def get_user_list():
     conn = sqlite3.connect('bd.sql')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM users')
+    cur.execute("SELECT name, surname FROM users")
     users = cur.fetchall()
-    info = ''
-    for el in users:
-        info += f'Имя: {el[1]}, пароль: {el[2]}\n'
-
     cur.close()
     conn.close()
-    bot.send_message(callback.message.chat.id, info)
+    return [f"{name} {surname}" for name, surname in users]
 
-<<<<<<< HEAD
 bot.polling(none_stop=True)
-=======
-bot.polling(none_stop=True)
->>>>>>> 74f2480f1dad03ae553317f76c9c9a75037401a1
